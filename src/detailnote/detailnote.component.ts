69016cc7e33ee3880c849e48e71dc0b0b3195075
CynthiaBudiono <c14180210@john.petra.ac.id>
2021-03-04 14:21:56 +0800

    1 file modified

--- src/detailnote/detailnote.component.ts
+++ src/detailnote/detailnote.component.ts
@@ -22,6 +22,7 @@ export class DetailnoteComponent implements OnInit {
   }
 
   goback(){
-    this.router.navigate(["/app/"]);
+    // this.router.navigate(["/app/"]);
+    this.router.navigate(["/"]);
   }
 }
